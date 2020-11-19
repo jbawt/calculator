3eb05490e47240e27ad340e754cf97d181ade616
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2020-11-19 00:19:28 -0700

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header">
+    <h1>Calculator App</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header {
+  width: 100%;
+  height: 10vh;
+  background-color: linear-gradient(to right rgba(49, 148, 102, 0.898), rgba(37, 145, 94, 0.967), rgba(49, 148, 102, 0.898));
+}
+</style>
